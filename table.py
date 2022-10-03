ff3343b11c064df827bdeef5ff0f943aafd44264
N3dal <nedalxzo@gmail.com>
2022-10-03 13:33:38 +0200

    update table.py

--- table.py
+++ table.py
@@ -5,24 +5,39 @@ Docs;
 """
 __version__ = 1.0
 
+from tools import Tools
+
 
 class Table:
     """
     draw Table on your terminal.
 
-    draw_element:str
+    draw_elements:str
         using this option you can change the draw elements that,
-        is used to draw the table the default are:
+        is used to draw the table the default depend on your operating system:
+
+          the default for window are:
             -> Horizontal-line: '-'
             -> Vertical-line: '|'
             -> Cross-point: '+'
             -> Special-Horizontal-Line for header: '='
+
+          the default for *nix are:
+            -
+
+            for windows use "WIN" and for other machines,
+            e.g. mac or linux use either "MAC" or "LINUX";
+
+            by default the table will be drawn using the windows,
+            elements;
 
         note: the Table will use only the four chars from any string.
             -> Horizontal-line: first-char of the string.
             -> Vertical-line: second-char of the string.
             -> Cross-point: third-char of the string.
             -> Special-Horizontal-Line for header: forth-char of the string.
+
+        note: pass an string separated by space chars;
 
         if you pass string with less than four elements the Table will use the
         defaults for the remain ones for ex:
@@ -46,28 +61,31 @@ class Table:
     #   ('+', '-', '|', '=')
     # and we can use some Special Uni-Code characters but this can make,
     # some bugs Especially for windows machines.
-    __DEFAULT_DRAW_ELEMENTS = "-|+="
+    __DEFAULT_DRAW_ELEMENTS = "- | + ="
+    __LINUX_DRAW_ELEMENTS = "╭ ─ ┬ ╮ │ ┼ ┴ ╰ ╯ ┤ ├"
+    __WINDOWS_DRAW_ELEMENTS = __DEFAULT_DRAW_ELEMENTS
 
-    def __init__(self, draw_elements=__DEFAULT_DRAW_ELEMENTS, special_header_line=False):
+    __DRAW_ELEMENTS_OPTIONS = ("WIN", "LINUX", "MAC")
 
-        self.draw_elements = draw_elements
+    __DEFAULT_OS = "WIN" if Tools.MACHINE_OS_NAME == "windows" else "LINUX"
+
+    def __init__(self, draw_element=__DEFAULT_OS, special_header_line=False):
+
         self.special_header_line = special_header_line
+
+        print(draw_element)
 
         # guard conditions
         assert isinstance(
-            draw_elements, str), f"{draw_elements} is {type(draw_elements)} only {str} is allowed "
+            draw_element, str), f"{draw_element} is {type(draw_element)} only {str} is allowed "
 
-        __draw_elements_len = len(draw_elements)
-        if __draw_elements_len < 4:
-            # even if the user pass an empty string this will use the default ones.
-            self.draw_elements += Table.__DEFAULT_DRAW_ELEMENTS[__draw_elements_len:]
+        assert draw_element in Table.__DRAW_ELEMENTS_OPTIONS, f"draw_element can only be one of {Table.__DRAW_ELEMENTS_OPTIONS}"
 
-        self.horizontal_line, self.vertical_line, self.cross_point, self.special_header_horizontal_line = self.draw_elements
+        if draw_element == "WIN":
+            self.__horizontal_line, self.__vertical_line, self.__cross_point, self.__special_header_horizontal_line = Table.__DEFAULT_DRAW_ELEMENTS.split()
+        else:
+            # for mac and linux machines;
+            pass
 
 
-t = Table(draw_elements="ABT*")
-
-print(t.horizontal_line)
-print(t.vertical_line)
-print(t.cross_point)
-print(t.special_header_horizontal_line)
+t = Table()
